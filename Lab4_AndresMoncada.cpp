3eac9b8c49c80a61753360e1f69ae4fec7cea841
ConejoMoncada <andresm1996@hotmail.com>
2018-08-10 16:07:19 -0600

    Corregir matriz ejercicio 2, todas las cochinas validaciones para poner una pieza

--- Lab4_AndresMoncada.cpp
+++ Lab4_AndresMoncada.cpp
@@ -9,6 +9,7 @@ void print(int**,int);
 void print(char**);
 void ej2();
 char** genChar();
+bool validar(char**,int,int);
 
 int main(){
 	srand(time(NULL));
@@ -126,6 +127,29 @@ void ej2(){
 	char** mc; //matriz de char
 	mc = genChar();
 	print(mc);
+	bool turno = true;
+	bool fin = false;
+	int f, c; //fila y columna
+	int el = 60;//espacios libres
+	while (!fin){
+		cout << "Turno de ";
+		if(turno)
+			cout << "X";
+		else
+			cout << "O";
+		cout << endl << "Ingrese una fila: ";
+		cin >> f;
+		cout << "Ingrese una columna: ";
+		cin >> c;
+		if (f == 0)
+			fin = true;
+		if(validar(mc,f,c))
+			cout << "YES";
+		else
+			cout << "NO";
+	}
+
+
 	for(int i = 0; i < 9; i++){
 		delete[] mc[i];
 	}
@@ -142,14 +166,15 @@ char** genChar(){
 			mc[i][j] = '.';
 		}
 	}
-	mc[0][1] = 'a';
-	mc[0][2] = 'b';
-	mc[0][3] = 'c';
-	mc[0][4] = 'd';
-	mc[0][5] = 'e';
-	mc[0][6] = 'f';
-	mc[0][7] = 'g';
-	mc[0][8] = 'h';
+	mc[0][0] = ' ';
+	mc[0][1] = '1';
+	mc[0][2] = '2';
+	mc[0][3] = '3';
+	mc[0][4] = '4';
+	mc[0][5] = '5';
+	mc[0][6] = '6';
+	mc[0][7] = '7';
+	mc[0][8] = '8';
 	mc[1][0] = '1';
 	mc[2][0] = '2';
 	mc[3][0] = '3';
@@ -158,10 +183,55 @@ char** genChar(){
 	mc[6][0] = '6';
 	mc[7][0] = '7';
 	mc[8][0] = '8';
-	mc[4][4] = 'N';
-	mc[5][5] = 'N';
-	mc[4][5] = 'B';
-	mc[5][4] = 'B';
+	mc[4][4] = 'X';
+	mc[5][5] = 'X';
+	mc[4][5] = 'O';
+	mc[5][4] = 'O';
 
 	return mc;
 }
+
+bool validar(char** mc, int f, int c){
+      bool v = false;
+      if (f > 0 && c > 0 && f <= 8 && c <= 8){
+	      if(mc[f][c] == '.'){
+		      if(f < 8){
+			      if(mc[f+1][c] == 'X' || mc[f+1][c] == 'O'){
+				      v = true;
+			      }//abajo
+			      else if(c < 8){
+				      if(mc[f+1][c+1] == 'X' || mc[f+1][c+1] == 'O')
+					      v = true;
+			      }//abajo derecha
+			      else if(c > 1){
+				      if(mc[f+1][c-1] == 'X' || mc[f+1][c-1] == 'O')
+					      v = true;
+			      }//abajo izquierda
+		      }//no está en la fila de abajo
+		      if(f > 1){
+			      if(mc[f-1][c] == 'X' || mc[f-1][c] == 'O'){
+				      v =true;
+			      }//arriba
+			      else if(c < 8){
+				      if(mc[f-1][c+1] == 'X'|| mc[f-1][c+1] == 'O')
+					      v = true;
+			      }//arriba derecha
+			      else if(c > 1){
+				      if(mc[f-1][c-1] == 'X'|| mc[f-1][c-1] == 'O')
+					      v = true;
+			      }//arriba izquierda
+		      }//no está en la fila de arriba
+		      if(c < 8){
+			      if(mc[f][c+1] == 'X' || mc[f][c+1] == 'O')
+				      v = true;
+		      }//derecha
+		      if(c > 1){
+			      if(mc[f][c-1] == 'X' || mc[f][c-1] == 'O')
+				      v = true;
+		      }//izquierda
+	      }//espacio no ocupado
+      }//f,c {1-8}
+      else
+	      cout << "Fila o columna fuera de rango" << endl;
+      return v;
+}      
